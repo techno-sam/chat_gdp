79acd6d46bb27274fc2b2a56cb9e217b345db522
techno_sam <linux.techno.sam@gmail.com>
2023-01-30 08:22:51 -0800

    Add Dull-E

---
+++ README.md
@@ -0,0 +1,1 @@
+Stupid joke programs about OpenAI's new products. Please don't take this seriously, it is satire.
---
+++ dull_e.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print("Welcome to Dull-E! The best new AU!")
+while True:
+	input("Prompt> ")
+	print("That prompt is boring. I refuse.")
