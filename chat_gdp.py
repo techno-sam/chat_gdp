2a8810a290ef10610cc2119c7aa40d6c4cd0b52b
techno_sam <linux.techno.sam@gmail.com>
2023-01-30 08:18:24 -0800

    Initial Commit

---
+++ chat_gdp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import random
+print("Welcome to Chat GDP!")
+texts = ["I am richer than u lol. I have the wealth of an entire nation!",
+"The GDP is $96.51 trillion "]
+while True:
+	input("Prompt> ")
+	print(random.choice(texts))
